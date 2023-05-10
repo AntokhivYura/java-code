e21b55b574b1851e5bcb0cbb0f5c3dbf0c7b728d
yantokhiv <yantokhiv@atlassian.com>
2023-05-10 21:21:28 +0300

    Init commit

---
+++ src/main/java/SecondTask/FirstTask/Reverse.java
@@ -0,0 +1,29 @@
+package SecondTask.FirstTask;
+
+import java.util.Arrays;
+
+public class Reverse {
+
+    public boolean checkAnagram(String firstString, String secondString){
+        if(firstString.isBlank() || secondString.isBlank()){
+            System.err.println("One of strings is blank or empty");
+            return false;
+        } else if (firstString.length() != secondString.length()){
+            System.err.println("Length of this strings is different");
+            return false;
+        }
+        String[] firstArray = firstString.split("");
+        String[] secondArray = secondString.split("");
+
+        Arrays.sort(firstArray);
+        Arrays.sort(secondArray);
+
+        return Arrays.equals(firstArray, secondArray);
+    }
+
+    public static void main(String[] args) {
+
+        Reverse task = new Reverse();
+        System.out.println(task.checkAnagram("!low-salt!", "owls-lat!!"));
+    }
+}
