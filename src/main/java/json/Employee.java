feb08c299b798634a2cbc8db2ba71ce09ae2ff83
Yurii <antokhivyura@gmail.com>
2023-05-11 13:11:45 +0300

    Added Employee object to parse JSON into this object

---
+++ src/main/java/json/Employee.java
@@ -0,0 +1,50 @@
+package json;
+
+public class Employee {
+    private String name;
+    private String sureName;
+    private String age;
+    private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", sureName='" + sureName + '\'' +
+                ", age='" + age + '\'' +
+                ", address=" + address.toString() +
+                '}';
+    }
+}
