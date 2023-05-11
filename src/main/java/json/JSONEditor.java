69a03a8b6ffcffc6e1cb7a325bd950e8d7303efb
Yurii <antokhivyura@gmail.com>
2023-05-11 13:12:43 +0300

    Added JSONEditor with functions to work with json file

---
+++ src/main/java/json/JSONEditor.java
@@ -0,0 +1,58 @@
+package json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+
+public class JSONEditor {
+    private final Gson gson;
+    private Employee employee;
+
+    public JSONEditor(File file) {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        try (Reader reader = new FileReader(file)) {
+            employee = gson.fromJson(reader, Employee.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeObjectToJSONFile(File file) {
+        try (FileWriter writer = new FileWriter(file)) {
+            gson.toJson(employee, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setEmployeeName(String newName) {
+        employee.setName(newName);
+    }
+
+    public void setEmployeeSureName(String newSureName) {
+        employee.setSureName(newSureName);
+    }
+
+    public void setEmployeeAge(String newAge) {
+        employee.setAge(newAge);
+    }
+
+    public void setEmployeeAddress(Address address) {
+        employee.setAddress(address);
+    }
+
+    public String getEmployee() {
+        return employee.toString();
+    }
+
+    public static void main(String[] args) {
+        File fileToJSON = new File("src/main/resources/Employee.json");
+        Address address = new Address("USA", "Texas");
+        JSONEditor editor = new JSONEditor(fileToJSON);
+
+        editor.writeObjectToJSONFile(fileToJSON);
+        System.out.println(editor.getEmployee());
+
+    }
+}
